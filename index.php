1ae1dcaba5dd2ecb6a455bb556e5f62a0feb55bc
Yohan Tahir <70063466+YohanTr@users.noreply.github.com>
2020-10-29 09:37:46 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once  'Vehicle.php';
+require_once 'Bicycle.php';
+require_once 'Car.php';
+require_once 'Truck.php';
+
+$bicycle = new Bicycle('blue', 1);
+echo $bicycle->forward();
+var_dump($bicycle);
+
+
+$car = new Car('green', 4, 'electric');
+echo $car->forward();
+var_dump($car);
+
+var_dump(Car::ALLOWED_ENERGY);
+
+$truck = new Truck(100, 'Blue', 2);
+var_dump($truck);
+$truck->setCurrentStock(30);
+echo $truck->filling();
+echo $truck->forward();
+echo $truck->brake();
